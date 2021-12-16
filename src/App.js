f0e7e5bbde172bcc42eed21422f48dfcffc398f6
AVirolainen <58146499+AVirolainen@users.noreply.github.com>
2021-12-17 01:04:56 +0200

    init and adding routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>React Redux App</h1>}></Route>
+        <Route path="/add" element={<h1>React Reeeeedux App</h1>}></Route>
+        <Route path="/edit/:id" element={<h1>React Redux App</h1>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+      <Link to="/" className="navbar-brand ml-5">
+        React Contact App
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
